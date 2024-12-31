dcfb0dc500979a380e62b2d3afeef0581761e4e1
abed1453 <120453105+abed1453@users.noreply.github.com>
2024-12-31 14:51:32 +0800

    Updates

---
+++ precache-manifest.5936dd816e23581556802aa738958a19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362b99e5be12ceb08d9518c28e74b571",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0be1559f977f24dee529",
+    "url": "/robofriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "40ea6a7e88453896b72a",
+    "url": "/robofriends/static/css/main.48e00e7b.chunk.css"
+  },
+  {
+    "revision": "0be1559f977f24dee529",
+    "url": "/robofriends/static/js/2.bf1445c8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/robofriends/static/js/2.bf1445c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be4e3c2d9a9910e3e93",
+    "url": "/robofriends/static/js/3.99fa299b.chunk.js"
+  },
+  {
+    "revision": "40ea6a7e88453896b72a",
+    "url": "/robofriends/static/js/main.f5ef3ddc.chunk.js"
+  },
+  {
+    "revision": "9150b77577b6515b3664",
+    "url": "/robofriends/static/js/runtime-main.a2489e9f.js"
+  }
+]);
